72c6cca061d6cd183a676f56723d21095d69fd2e
hoge1e3 <hoge1e3@gmail.com>
2019-01-16 13:44:50 +0900

    another runtime

--- js/generatorifyRuntime0.js
+++ js/generatorifyRuntime0.js
@@ -1,61 +1,38 @@
 //define([],function () {
 (function () {
-    function F(f) {
-        var src=f+"";
-        var argsp=/^function\s*\(([^\)]+)\)/;
-        var args=argsp.exec(src)[1].replace(/\s/g,"").split(",");
-        var bodyp=/\/\*---((\n|\r|.)*)---\*\//;
-        try {
-            return Function.apply(null, args.concat([ bodyp.exec(src)[1] ]));
-        } catch (e) {
-            return f;
-        }
+    var SYMBOL_G="SYMBOL_G";
+    function G(o) {
+        o[SYMBOL_G]=true;
+        return o;
     }
-    var Context=function () {
-        this.WrappedGenerator=function(gen){this.gen=gen;};
-    };
-    var p=Context.prototype;
-    p.wrapFunction=function (f) {
-        //f:GeneratorFunction
-        var t=this;
-        return (function () {
-            var res=f.apply(this,arguments);
-            return t.wrapGenerator(res);
-        });
-    };
-    p.isWrappedGenerator=function (v) {
-        return (v instanceof this.WrappedGenerator);
-    };
-    p.wrapGenerator=function (v) {
-        // v:Generator
-        return new this.WrappedGenerator(v);
-    };
-    p.call=F(function (v) {
-        if (this.isWrappedGenerator(v)) return v.gen;
-        if (v instanceof WrappedPromise) {
-            var res,err,iserr;
-            v.nativePromise.then(function (r) {
-                res=r;
-            },function (e) {
-                err=e;
-                iserr=true;
-            });
-            return (function*(){
-                yield v;
-                if (iserr) throw err;
-                return res;
-            })();
-        }
-        return (function*(){return v;})();
-    });
-    var WrappedPromise=function (succ,err) {
-        this.nativePromise=new Promise(succ,err);
-    };
     generatorifyRuntime={
-        Context:Context,
-        promise: function (succ,err) {
-            return new WrappedPromise(succ,err);
+        yieldable: function (gf) {
+            return G(gf);
+        },
+        isGenerator: function isGenerator(v) {
+            return v && v[SYMBOL_G];
+        },
+        toGen: F(function (v) {
+            if (this.isGenerator(v)) {
+                return v;
+            }
+            return (function*(){return v;})();
+        }),
+        gToVal: function (genF,thiz) {
+            return this.toVal(genF.call(thiz));
+        },
+        toVal: function (gen) {
+            var n=gen.next();
+            if (n.done) return n.value;
+            return G((function*() {
+                while(true) {
+                    yield n.value;
+                    n=gen.next();
+                    if (n.done) return n.value;
+                }
+            })());
         }
     };
+    //generatorifyRuntime.init();
     return generatorifyRuntime;
 })();
